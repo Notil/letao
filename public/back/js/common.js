7dc0e1a3ef8966d9a69a3c6c665d81112c6fbc22
ALLENwARE <565323447@qq.com>
2018-06-25 22:03:49 +0800

    公共部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,48 @@
 /**
  * Created by ALLENWARE on 2018/6/25.
  */
+
+if(location.href.indexOf("login.html")=== -1){
+  $.ajax({
+    type:"get",
+    url:" /employee/employeeLogout",
+    dataType:'json',
+    success:function ( info ) {
+        if(info.error === 400){
+          location.href = "login.html";
+        }
+    }
+  })
+}
+
+
+
+$(document).ajaxStart(function () {
+    NProgress.start();
+})
+
+$(document).ajaxStop(function () {
+  setTimeout(function () {
+    NProgress.done();
+
+  },5000)
+});
+
+
+$('.category').click(function () {
+    $('.child').stop().slideToggle();
+})
+
+$('.icon_menu').click(function () {
+    $('.lt_aside').toggleClass('hidemeau');
+    $('.lt_main').toggleClass('hidemeau');
+    $('.lt_main .nav').toggleClass('hidemeau')
+})
+
+$('.icon_logout').click(function () {
+    $('#logoutModal').modal("show");
+})
+
+$('#logoutBtn').click(function () {
+    location.href = "login.html";
+})
